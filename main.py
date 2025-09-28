1bb400bfc3c6d8780211c111e5669f8e36cd221f
PDGamerSG <pallab4586das@gmail.com>
2025-09-28 18:26:36 +0530

    Fixed issue #3

--- main.py
+++ main.py
@@ -45,15 +45,10 @@ while True:
 
     if game.game_over:
         if game.score == 0:
-            draw_start_screen(screen, title_font)
-            score_value = title_font.render("Score: "+str(game.score), True, Colors.white)
-            screen.blit(score_value, (195, 295))
-            pygame.display.update()
+            draw_start_screen(screen, title_font,game.score,(195,295))
         else:
-            draw_game_over_screen(screen, title_font)
-            score_value = title_font.render("Score: "+str(game.score), True, Colors.white)
-            screen.blit(score_value, (180, 295))
-            pygame.display.update()
+            draw_game_over_screen(screen, title_font,game.score,(180, 295))
+        pygame.display.flip()
     else:
         score_value_surface = title_font.render(str(game.score), True, Colors.white)
         screen.fill(Colors.dark_blue)
@@ -63,5 +58,5 @@ while True:
         screen.blit(score_value_surface, score_value_surface.get_rect(centerx=score_rect.centerx, centery=score_rect.centery))
         pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
         game.draw(screen)
-        pygame.display.update()
+        pygame.display.flip()
     clock.tick(60)
